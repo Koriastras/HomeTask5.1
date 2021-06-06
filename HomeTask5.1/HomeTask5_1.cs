c2aa1c37a6e4fb49f63b1d3e14254f8f2e253ebd
Yuliia Mysova-Shreder <66831680+Koriastras@users.noreply.github.com>
2021-06-06 09:21:41 +0300

    Update HomeTask5_1.cs

--- HomeTask5.1/HomeTask5_1.cs
+++ HomeTask5.1/HomeTask5_1.cs
@@ -12,6 +12,9 @@ namespace HomeTask5._1
     {
 
         IWebDriver driver;
+        public static ExtentTest test;
+        public static ExtentReports extent;
+
 
         [OneTimeSetUp]
 
@@ -20,6 +23,12 @@ namespace HomeTask5._1
 
             driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
+            extent = new ExtentReports();
+            var htmlreporter = new ExtentHtmlReporter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory));
+            
+            extent.AttachReporter(htmlreporter);
+            extent.AddSystemInfo("Host Name", "Yuliia");
+            extent.AddSystemInfo("User Name", "Yuliia Mysova-Shreder");
 
         }
 
@@ -28,17 +37,31 @@ namespace HomeTask5._1
         public void AfterTest()
         {
             driver.Quit();
+
+            extent.Flush();
+
         }
 
 
         [Test]
+
+       
+
         public void LoginProcess()
         {
+
+            test = null;
+            test = extent.CreateTest("HomeTask5.1").Info("Registration process");
+
             var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
             string basePageUrl = "https://www.demoblaze.com/index.html";
+
+
             driver.Navigate().GoToUrl(basePageUrl);
 
             wait.Until(x => x.FindElement(By.Id("nava")).Displayed);
+
+            test.Log(Status.Info, "Go to URL");
 
             IWebElement loginButton = driver.FindElement(By.Id("login2"));
             IWebElement loginButtonXPath = driver.FindElement(By.XPath("//a[@data-target = '#logInModal']"));
@@ -56,6 +79,8 @@ namespace HomeTask5._1
             usernameBox.Clear();
             usernameBoxXPath.SendKeys("KoriAstras");
 
+            
+
             IWebElement passwordBox = driver.FindElement(By.Id("loginpassword"));
             IWebElement passwordBoxXPath = driver.FindElement(By.XPath("//input[@id = 'loginpassword' and @type = 'password']"));
             IWebElement passwordBoxCss = driver.FindElement(By.CssSelector("input[type = 'password'][id = 'loginpassword']"));
@@ -63,20 +88,37 @@ namespace HomeTask5._1
             passwordBox.Clear();
             passwordBoxCss.SendKeys("Kori123!");
 
+            test.Log(Status.Info, "Enter username and password");
+
             IWebElement submitBoxXPath = driver.FindElement(By.XPath("//button[@onclick = 'logIn()']"));
             IWebElement submitBoxCss = driver.FindElement(By.CssSelector("button[onclick = 'logIn()']"));
 
 
             submitBoxXPath.Click();
 
-            wait.Until(x => x.FindElement(By.Id("logout2")).Displayed);
-            Assert.True(driver.FindElement(By.XPath("//a[@onclick = 'logOut()']")).Displayed, "Logout button is not displayed");
 
-            string nameOfUser = driver.FindElement(By.CssSelector("#nameofuser")).Text;
-            Assert.True(driver.FindElement(By.XPath("//*[@id = 'nameofuser']")).Displayed, "Name of user is not displayed");
 
-            Assert.AreEqual(nameOfUser, "Welcome KoriAstras", "Username is different");
+            try
+            {
+                wait.Until(x => x.FindElement(By.Id("logout2")).Displayed);
+                Assert.True(driver.FindElement(By.XPath("//a[@onclick = 'logOut()']")).Displayed, "Logout button is not displayed");
+
+                string nameOfUser = driver.FindElement(By.CssSelector("#nameofuser")).Text;
+                Assert.True(driver.FindElement(By.XPath("//*[@id = 'nameofuser']")).Displayed, "Name of user is not displayed");
+
+                Assert.AreEqual(nameOfUser, "Welcome KoriAstras", "Username is different");
+
+                test.Log(Status.Pass, "Test Pass, Username is correct");
+
+            }
+
+            catch
+            {
+                test.Log(Status.Fail, "Username is different");
+
+                throw;
+
+            }
         }
-
     }
 }
